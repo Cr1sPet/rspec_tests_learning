9b7c1c71a161f0ee058b68b06a5078cb1385926b
Cr1sPet <serezha.petrov.02@gmail.com>
2022-12-12 11:32:02 +0300

    added utils define_derived_metadata to the Rspec.configure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
 require_relative '../lib/exchange_it'
+
+RSpec.configure do |config|
+
+  config.define_derived_metadata(:file_path => %r{/exchange_it/utils}) do |metadata|
+    metadata[:utils] = true
+  end
+end
