3478bc5548abebf3dcedf86dd482bbc2785b1d2f
Cr1sPet <serezha.petrov.02@gmail.com>
2022-12-12 11:52:40 +0300

    example_status_persistence config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,9 @@
 require_relative '../lib/exchange_it'
 
 RSpec.configure do |config|
+  config.example_status_persistence_file_path = 'spec/examples.txt'
 
-  config.define_derived_metadata(:file_path => %r{/exchange_it/utils}) do |metadata|
+  config.define_derived_metadata(file_path: %r{/exchange_it/utils}) do |metadata|
     metadata[:utils] = true
   end
 end
