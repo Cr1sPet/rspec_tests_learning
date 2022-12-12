e97a1581fc3512d08167017609ddec15a6f17c8f
Cr1sPet <serezha.petrov.02@gmail.com>
2022-12-12 11:51:07 +0300

    added metadata usage to the user_spec.rb

--- spec/lib/exchange_it/user_spec.rb
+++ spec/lib/exchange_it/user_spec.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe ExchangeIt::User do
-  let(:user) { described_class.new 'Bob', 'Marlie' }
-  let(:user_no_string_name) { described_class.new nil, 'Marlie' }
+  let(:user) { |ex| described_class.new(ex.metadata[:name] || 'Bob', ex.metadata[:lastname] || 'Marlie') }
   let(:user_no_string_lastname) { described_class.new 'Bob', 10 }
 
   it 'returns correct name' do
@@ -13,11 +12,11 @@ RSpec.describe ExchangeIt::User do
     expect(user.lastname).to eq('Marlie')
   end
 
-  it 'returns name as a String' do
-    expect(user_no_string_name.name).to be_an_instance_of(String)
+  it 'returns name as a String', name: nil do
+    expect(user.name).to be_an_instance_of(String)
   end
 
-  it 'returns lastname as a String' do
+  it 'returns lastname as a String', lastname: 10 do
     expect(user_no_string_lastname.lastname).to be_an_instance_of(String)
   end
 
